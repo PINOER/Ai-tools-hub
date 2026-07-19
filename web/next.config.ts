3edc23848270a1ce0807379460a695accfab61ac
PINOER <K162000080@gmail.com>
2026-07-19 20:08:02 +0800

    Update next.config.ts

--- web/next.config.ts
+++ web/next.config.ts
@@ -22,3 +22,10 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
+const nextConfig = {
+  images: {
+    unoptimized: true, // 极其重要：禁用 Next.js 默认图片优化，改用浏览器原生渲染
+  }
+};
+module.exports = nextConfig;
